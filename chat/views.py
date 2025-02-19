6d512530b553b496ed47e877a2f2f7159b4df97e
Gabriel Montalvão Santos <gabriel.msantos7@hotmail.com>
2025-02-18 22:13:42 -0300

    Atulização de coisas

--- chat/views.py
+++ chat/views.py
@@ -8,12 +8,15 @@ from .api import call
 async def index(request):
     if request.method == 'POST':
         form = EnvioArquivo(request.POST, request.FILES)
-        
         if form.is_valid():
             arquivo = request.FILES['arquivo']
             destino = os.path.join(settings.MEDIA_ROOT, arquivo.name)
             print("Salvando em:", destino)
             os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
+            
+            with open(destino, 'wb+') as f:
+                for chunk in arquivo.chunks():
+                    f.write(chunk)
 
             # Chamada assíncrona do chat
             ai_response = await asyncio.to_thread(call, arquivo.name)
